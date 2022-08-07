b4051737f8ed04c6317d374ebbc0d504cbf4ccbc
Zac Crabtree <zac.crabtree1@gmail.com>
2022-08-06 22:15:42 -0500

    initial commit

---
+++ todo.js
@@ -0,0 +1,25 @@
+let id = 0;
+
+document.getElementById("add").addEventListener("click", () => {
+    let createdDate = new Date();
+    let table = document.getElementById("list");
+    let row = table.insertRow(1);
+    row.setAttribute("id", `item-${id}`);
+    row.insertCell(0).innerHTML = document.getElementById("new-task").value;
+    row.insertCell(1).innerHTML = document.getElementById("new-start-date").value;
+    row.insertCell(2).innerHTML = document.getElementById("new-end-date").value;
+    let actions = row.insertCell(3);
+    actions.appendChild(createDeleteButton(id++));
+    document.getElementById("new-task").value = "";
+})
+function createDeleteButton(id) {
+    let btn = document.createElement("button");
+    btn.className = "btn btn-primary";
+    btn.id = id;
+    btn.innerHTML = "Delete";
+    btn.onclick = () => {
+        let elementToDelete = document.getElementById(`item-${id}`)
+        elementToDelete.parentNode.removeChild(elementToDelete);
+    }   
+   return btn; 
+}
